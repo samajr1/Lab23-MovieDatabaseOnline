663a7ffbdfe4f36dbb2292a4c6eeaa91787792d4
Sam <Sam.Ableson.Jr@gmail.com>
2016-09-06 17:23:28 -0400

    updated essential files

---
+++ src/DBSettings.java
@@ -0,0 +1,5 @@
+public class DBSettings {
+	public static String url = "jdbc:mysql://localhost:3306/movies?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+	public static String user = "jdbc";//username I set up
+	public static String pass = "admin";//password I set up
+}
